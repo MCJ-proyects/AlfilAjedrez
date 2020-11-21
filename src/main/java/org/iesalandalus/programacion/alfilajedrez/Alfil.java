a7a57ee9ebabcf5d77fd92542000c9839ebbc929
CJ <CJ@DESKTOP-OS2TG00>
2020-11-21 14:33:39 +0100

    He creado los metodos get y set para cada atributo, comprobando la validez de estos e incluyendo la excepcion en caso de que no sea correcto (apartado 12)

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -5,4 +5,22 @@ public class Alfil {
 	private Color color;
 	private Posicion posicion;
 	
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if (color != Color.BLANCO || color != Color.NEGRO) {
+			throw new IllegalArgumentException("ERROR: El color no es válido.");
+		}
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+	
+	
+	
 }
